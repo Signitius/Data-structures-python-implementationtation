d875b1f6be431653997a76d0e48c52deef9cb3d9
Signitius <154023435+Signitius@users.noreply.github.com>
2026-06-30 17:10:36 +0300

    Update stack.py
    initial

--- datastructures/stack.py
+++ datastructures/stack.py
@@ -1,1 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.items = []
 
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Pop from empty stack")
+        return self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from empty stack")
+        return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+        
+        
+d=Stack()
+d.push(9)
+d.push(4)
+print(d.items)
+print(d.peek())
